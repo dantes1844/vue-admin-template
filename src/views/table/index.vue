19cbb796818eddea0ee585cfb95b8ba702f805b0
laobai <xinbucun@qq.com>
2020-12-18 19:48:13 +0800

    增加操作按钮及方法

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -6,8 +6,7 @@
       element-loading-text="Loading"
       border
       fit
-      highlight-current-row
-    >
+      highlight-current-row>
       <el-table-column align="center" label="ID" width="95">
         <template slot-scope="scope">
           {{ scope.$index }}
@@ -28,41 +27,78 @@
           {{ scope.row.pageviews }}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+      <el-table-column
+        class-name="status-col"
+        label="Status"
+        width="110"
+        align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+          <el-tag :type="scope.row.status | statusFilter">{{
+            scope.row.status
+          }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column
+        align="center"
+        prop="created_at"
+        label="Display_time"
+        width="200">
         <template slot-scope="scope">
           <i class="el-icon-time" />
           <span>{{ scope.row.display_time }}</span>
         </template>
       </el-table-column>
+      <el-table-column
+        label="操作"
+        align="center"
+        width="230"
+        class-name="small-padding fixed-width">
+        <template slot-scope="{ row, $index }">
+          <el-button
+            type="primary"
+            size="mini"
+            @click="handleUpdate(row)">编辑</el-button>
+          <el-button
+            v-if="row.status != 'published'"
+            size="mini"
+            type="success"
+            @click="handleModifyStatus(row, 'published')">发布</el-button>
+          <el-button
+            v-if="row.status != 'draft'"
+            size="mini"
+            @click="handleModifyStatus(row, 'draft')">下架</el-button>
+          <el-button
+            v-if="row.status != 'deleted'"
+            size="mini"
+            type="danger"
+            @click="handleDelete(row, $index)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
-    <pagination v-show="total>0"
+    <pagination
+      v-show="total > 0"
       :total="total"
       :page.sync="listQuery.page"
       :limit.sync="listQuery.limit"
-      @pagination="getList" />
-
+      @pagination="getList"
+    />
   </div>
 </template>
 
 <script>
-import { getArticleList } from '@/api/table'
-import Pagination from '@/components/Pagination' // secondary package based on el-pagination
+import { getArticleList } from "@/api/table";
+import Pagination from "@/components/Pagination";
 
 export default {
   components: { Pagination },
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
+        published: "success",
+        draft: "gray",
+        deleted: "danger"
+      };
+      return statusMap[status];
     }
   },
   data() {
@@ -76,22 +112,37 @@ export default {
         importance: undefined,
         title: undefined,
         type: undefined,
-        sort: '+id'
-      },
-    }
+        sort: "+id"
+      }
+    };
   },
   created() {
-    this.getList()
+    this.getList();
   },
   methods: {
-    getList(){
+    getList() {
       this.listLoading = true;
       getArticleList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.listLoading = false,
-        this.total = response.data.total;
-      })
+        this.list = response.data.items;
+        (this.listLoading = false), (this.total = response.data.total);
+      });
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: "操作成功",
+        type: "success"
+      });
+      row.status = status;
+    },
+    handleDelete(row, index) {
+      this.$notify({
+        title: "成功",
+        message: "删除成功",
+        type: "success",
+        duration: 2000
+      });
+      this.list.splice(index, 1);
     }
   }
-}
+};
 </script>
