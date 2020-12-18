1c102688f33bf90990ca603052470c96ab63ce0c
laobai <xinbucun@qq.com>
2020-12-18 20:54:24 +0800

    增加新增的按钮和表单

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -44,17 +44,26 @@
           <el-radio label="Venue" />
         </el-radio-group>
       </el-form-item>
-      <el-col :span="24">
-        <el-form-item>
-                <el-button
-                    type="primary"
-                    :size="controlSize"
-                    @click="onSubmit">查询</el-button>
-                <el-button
-                    :size="controlSize"
-                    @click="onCancel">取消</el-button>
-        </el-form-item>
-      </el-col>
+      <el-row>
+        <el-col :span="23">
+          <el-form-item>
+                  <el-button
+                      type="primary"
+                      :size="controlSize"
+                      @click="onSubmit">查询</el-button>
+                  <el-button
+                      :size="controlSize"
+                      @click="onCancel">取消</el-button>
+          </el-form-item>
+        </el-col>
+        <el-col :span="1">
+            <el-button
+                type="primary"
+                :size="controlSize"
+                @click="handleCreate">新增</el-button>
+        </el-col>
+      </el-row>
+
     </el-form>
     <el-table
       v-loading="listLoading"
@@ -136,8 +145,49 @@
       :total="total"
       :page.sync="listQuery.page"
       :limit.sync="listQuery.limit"
-      @pagination="getList"
-    />
+      @pagination="getList"/>
+
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form
+        ref="dataForm"
+        :rules="rules"
+        :model="temp"
+        :size="controlSize"
+        label-position="left"
+        label-width="70px"
+        style="width: 400px; margin-left:50px;">
+        <el-form-item label="日期" prop="timestamp">
+          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />
+        </el-form-item>
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="temp.title" />
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
+            <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="重要性">
+          <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button
+          :size="controlSize"
+          @click="dialogFormVisible = false">
+          取消
+        </el-button>
+        <el-button
+          type="primary"
+          :size="controlSize"
+          @click="dialogStatus==='create'?createData():updateData()">
+          保存
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -162,6 +212,7 @@ export default {
       total: 0,
       list: null,
       listLoading: true,
+      dialogFormVisible: false,
       listQuery: {
         page: 1,
         limit: 20,
@@ -179,7 +230,27 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      }
+      },
+      temp: {
+        id: undefined,
+        importance: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        type: '',
+        status: 'published'
+      },
+      dialogStatus: '',
+      statusOptions: ['published', 'draft', 'deleted'],
+      textMap: {
+        update: 'Edit',
+        create: 'Create'
+      },
+      rules: {
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+        title: [{ required: true, message: 'title is required', trigger: 'blur' }]
+      },
     };
   },
   created() {
@@ -204,6 +275,71 @@ export default {
         type: "success"
       });
       row.status = status;
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        importance: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        status: 'published',
+        type: ''
+      }
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
+          this.temp.author = 'vue-element-admin'
+          createArticle(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '创建成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+          updateArticle(tempData).then(() => {
+            const index = this.list.findIndex(v => v.id === this.temp.id)
+            this.list.splice(index, 1, this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '更新成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
     },
     handleDelete(row, index) {
       this.$notify({
